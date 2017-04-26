fa793d1363c9b33ef7bbcb1228d9c05486a09b93
xiricek <xiricek@nymfe36.fi.muni.cz>
2017-04-26 12:04:52 +0200

    Creating_project

---
+++ Evidence_prijmu_a_vydaju/src/evidence_prijmu_a_vydaju/Evidence_prijmu_a_vydaju.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidence_prijmu_a_vydaju;
+
+/**
+ *
+ * @author xiricek
+ */
+public class Evidence_prijmu_a_vydaju {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
