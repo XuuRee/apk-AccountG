110321bd360044b034ea7ed99dd96d81d5bce039
xiricek <xiricek@nymfe35.fi.muni.cz>
2017-05-03 12:37:38 +0200

    payment_entity

---
+++ Evidence_prijmu_a_vydaju/src/evidence_prijmu_a_vydaju/backend/Payment.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidence_prijmu_a_vydaju.backend;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author xiricek
+ */
+public class Payment {
+    
+    private Long id;
+    private BigDecimal amount;
+    private LocalDate date;
+    private PaymentType type;
+    private String info;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public PaymentType getType() {
+        return type;
+    }
+
+    public void setType(PaymentType type) {
+        this.type = type;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Payment other = (Payment) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return  type  + ": " + "amount=" + amount + ", date=" + date + ", info=" + info + '}';
+    }  
+}
---
+++ Evidence_prijmu_a_vydaju/src/evidence_prijmu_a_vydaju/backend/PaymentType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidence_prijmu_a_vydaju.backend;
+
+/**
+ *
+ * @author xiricek
+ */
+public enum PaymentType {
+    INCOME,
+    EXPENSE;
+}
