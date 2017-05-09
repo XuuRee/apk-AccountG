77ef58ce5bc299e2428f9f00e6119d0cc1b8b090
Lukáš Suchánek <433654@mail.muni.cz>
2017-05-09 14:37:25 +0200

    manager added

---
+++ Evidence_prijmu_a_vydaju/src/evidence_prijmu_a_vydaju/backend/Manager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidence_prijmu_a_vydaju.backend;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Manager {
+    /**
+     * 
+     * @param year 
+     */
+    public void startYear(int year){
+    
+    }
+    /**
+     * 
+     * @param year 
+     */
+    public void endYear(int year){
+    
+    }
+    /**
+     * 
+     * @param payment 
+     */
+    public void registerPayment(Payment payment){
+    
+    }
+    /**
+     * 
+     * @param year 
+     */
+    public void countPayments(int year){
+    
+    }
+    
+}
